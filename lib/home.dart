a54809e25307a64fff17ebaa8344c4d8fbfece9e
zalim-388 <jofilx388@gmail.com>
2025-01-06 13:40:09 +0530

    3

--- lib/home.dart
+++ lib/home.dart
@@ -12,18 +12,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late TextEditingController controller;
-
-  @override
-  void initate() {
-    super.initState();
-    controller = TextEditingController();
-  }
-
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController placeController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
   @override
   void dispose() {
     super.dispose();
-    controller.dispose();
+    nameController.dispose();
+    ageController.dispose();
+    placeController.dispose();
+    phoneController.dispose();
   }
 
   @override
@@ -31,23 +30,106 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(''),
+        title: Text('User Detail'),
       ),
       body: Center(
-        child: Consumer<model>(builder: (context, model, child) {
-          return Column(
-            children: [
-              Text(
-                'Name:${model.name}',
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
+        child: Consumer<CounterModel>(builder: (context, model, child) {
+          return ListView.builder(
+            itemCount: model.details.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final item = model.details[index];
+
+              return GestureDetector(
+                onLongPress: () => model.remove(item),
+                child: Container(
+                    child: ListTile(
+                  leading: CircleAvatar(child: Text(item['name']![0])),
+                  title: Text(
+                    item['name'] ?? 'no name',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  subtitle: Text(
+                    'Age:${item['age'] ?? "N\A"} place:${item['place']}, phone:${item['phone']} ',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                )),
+              );
+            },
           );
+          //
+          //
+          //
+          //
+          // Column(
+          //   children: [
+          //     SizedBox(
+          //       height: 40,
+          //     ),
+          //     Container(
+          //       height: 150,
+          //       width: 200,
+          //       decoration: BoxDecoration(
+          //           border: Border.all(
+          //             color: Colors.black,
+          //           ),
+          //           borderRadius: BorderRadius.all(Radius.circular(20))),
+          //       child: Padding(
+          //         padding: const EdgeInsets.symmetric(horizontal: 15),
+          //         child: Column(
+          //           children: [
+          //             SizedBox(
+          //               height: 10,
+          //             ),
+          //             Align(
+          //               alignment: Alignment.centerLeft,
+          //               child: Text(
+          //                 'Name:${model?.name}',
+          //                 style: TextStyle(fontSize: 20),
+          //               ),
+          //             ),
+          //             SizedBox(
+          //               height: 5,
+          //             ),
+          //             Align(
+          //               alignment: Alignment.centerLeft,
+          //               child: Text(
+          //                 'Age:${model?.age}',
+          //                 style: TextStyle(fontSize: 20),
+          //               ),
+          //             ),
+          //             SizedBox(
+          //               height: 5,
+          //             ),
+          //             Align(
+          //               alignment: Alignment.centerLeft,
+          //               child: Text(
+          //                 'place:${model?.place}',
+          //                 style: TextStyle(fontSize: 20),
+          //               ),
+          //             ),
+          //             SizedBox(
+          //               height: 5,
+          //             ),
+          //             Align(
+          //               alignment: Alignment.centerLeft,
+          //               child: Text(
+          //                 'phone:${model?.phone}',
+          //                 style: TextStyle(fontSize: 20),
+          //               ),
+          //             ),
+          //           ],
+          //         ),
+          //       ),
+          //     ),
+          //   ],
+          // );
         }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          opendialog(context);
+          opendialog(context, nameController, ageController, placeController,
+              phoneController);
         },
         child: const Icon(Icons.add),
       ),
@@ -55,81 +137,100 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Future opendialog(BuildContext context) => showDialog(
-    context: context,
-    builder: (Context) => SimpleDialog(children: [
-          Column(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: TextField(
-                  onChanged: (value) {
-                    model().chagename(value);
-                  },
-
-                  // controller: TextEditingController(),
-                  decoration: InputDecoration(
-                    hintText: 'Enter the name',
-                    labelText: 'Name',
+Future opendialog(
+        BuildContext context,
+        TextEditingController nameController,
+        TextEditingController ageController,
+        TextEditingController placeController,
+        TextEditingController phoneController) =>
+    showDialog(
+        context: context,
+        builder: (Context) => SimpleDialog(children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: TextField(
+                      // onChanged: (value) {
+                      //   CounterModel().changeName(value);
+                      // },
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter the name',
+                        labelText: 'Name',
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: TextField(
-                  controller: TextEditingController(),
-                  decoration: InputDecoration(
-                    hintText: 'Enter your age',
-                    labelText: 'Age',
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: TextField(
+                      // onChanged: (value) {
+                      //   CounterModel().changeage(value);
+                      // },
+                      controller: ageController,
+                      decoration: InputDecoration(
+                          hintText: 'Enter your age', labelText: 'Age'),
+                      keyboardType: TextInputType.number,
+                    ),
                   ),
-                ),
-              ),
-              // Padding(
-              //   padding:
-              //       const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              //   child: TextField(
-              //     controller: TextEditingController(),
-              //     decoration: InputDecoration(
-              //       hintText: 'Enter your email',
-              //       labelText: 'Email',
-              //     ),
-              //   ),
-              // ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: TextField(
-                  controller: TextEditingController(),
-                  decoration: InputDecoration(
-                    hintText: 'Enter your place',
-                    labelText: ' place',
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: TextField(
+                      // onChanged: (value) {
+                      //   CounterModel().changeplace(value);
+                      // },
+                      controller: placeController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your place',
+                        labelText: ' place',
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: TextField(
-                  controller: TextEditingController(),
-                  decoration: InputDecoration(
-                    hintText: 'Enter your phone number',
-                    labelText: 'Phone number',
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: TextField(
+                      // onChanged: (value) {
+                      //   CounterModel().changephone(value);
+                      // },
+                      controller: phoneController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your phone number',
+                        labelText: 'Phone number',
+                      ),
+                      keyboardType: TextInputType.phone,
+                      maxLength: 10,
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 180),
-                child: TextButton(
-                    onPressed: () => submit(context), child: Text('Submit')),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 180),
+                    child: TextButton(
+                        onPressed: () => submit(context, nameController,
+                            ageController, placeController, phoneController),
+                        child: Text('Submit')),
+                  )
+                ],
               )
-            ],
-          )
-        ]));
-void submit(BuildContext context) {
+            ]));
+void submit(
+    BuildContext context,
+    TextEditingController nameController,
+    TextEditingController ageController,
+    TextEditingController placeController,
+    TextEditingController phoneController) {
+  final model = Provider.of<CounterModel>(context, listen: false);
+  model.addDetail(nameController.text, ageController.text, placeController.text,
+      phoneController.text);
+  // model.changeName(nameController.text);
+  // model.changeage(ageController.text);
+  // model.changeplace(placeController.text);
+  // model.changephone(phoneController.text);
+
   Navigator.of(context).pop();
 }
--- lib/provider.dart
+++ lib/provider.dart
@@ -1,11 +1,54 @@
 import 'package:flutter/material.dart';
 
 class CounterModel with ChangeNotifier {
-  String _name = '';
-  String get name => _name;
+  List<Map<String, String>> _details = [];
 
-  void changeName(String newName) {
-    _name = newName;
+  List<Map<String, String>> get details => List.unmodifiable(_details);
+
+  void remove(Map<String,String> item) {
+    _details.remove(item);
+    notifyListeners();
+  }
+
+  void addDetail(String name, String age, String place, String phone) {
+    if(name.isNotEmpty && age.isNotEmpty&& place.isNotEmpty && phone.isNotEmpty)
+    _details.add({
+      'name': name,
+      'age': age,
+      'place': place,
+      'phone': phone,
+    });
     notifyListeners();
   }
 }
+
+//   String _name = "";
+//   String _age = "";
+//   String _phone = "";
+//   String _place = "";
+
+//   String get name => _name;
+//   String get age => _age;
+//   String get phone => _phone;
+//   String get place => _place;
+
+//   void changeName(String newName) {
+//     _name = newName;
+//     notifyListeners();
+//   }
+
+//   void changeage(String age) {
+//     _age = age;
+//     notifyListeners();
+//   }
+
+//   void changeplace(String place) {
+//     _place = place;
+//     notifyListeners();
+//   }
+
+//   void changephone(String phone) {
+//     _phone = phone;
+//     notifyListeners();
+//   }
+// }
